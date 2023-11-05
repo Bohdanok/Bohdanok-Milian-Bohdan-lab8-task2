8eb2eff759306faa2db6534fedbc08dddeb7e837
Bohdan Milian <bohdan.milian@gmail.com>
2023-11-05 19:30:00 +0200

    Check rows

--- puzzle.py
+++ puzzle.py
@@ -7,13 +7,21 @@ def validate_board(board:list)->bool:
 "     9 5 ", " 6  83  *", "3   0  **", "  8  2***", "  2  ****"])
     True
     '''
-    # Check colums for same numbers
+    # Check colums
     for i in range(9):
         memory_list=[]
         for j in range(9):
             if board[j][i] !='*'and board[j][i] !=' ':
                 memory_list.append(board[j][i])
                 if board[j][i] in memory_list[:-1]:
+                    return False
+    # Check rows
+    for i in board:
+        memory_list=[]
+        for j in i:
+            if j !='*' and j!=' ':
+                memory_list.append(j)
+                if j in  memory_list[:-1]:
                     return False
 if __name__=='__main__':
     import doctest
